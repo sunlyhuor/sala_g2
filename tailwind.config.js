adf422b7d548d8bf5e61baffc9f85741d1d4063f
chheang-pheaktra <chheangpheaktra154@gmail.com>
2023-11-30 16:32:59 +0700

    config library

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,89 @@
 module.exports = {
   content: ["public/**/*.{html,js}"],
   theme: {
-    extend: {},
+    screens:{
+      sm:'640px',
+      md:'768px',
+      lg:'976px',
+      xl:'1280px'
+    },
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      'primary': '#1e1b4b',
+      'secondary': '#fcd34d',
+      'Onsecondary':'#fef08a',
+      'Onprimary':'#f4f4f5',
+      'Accent':'#7dd3fc',
+      'background':'#f9fafb',
+    },
+    fontSize:{
+      'header1': ['96px', {
+        lineHeight: '144px',
+        fontWeight: '700',
+      }],
+      'header2':['64px',
+        {
+          lineHeight:'96px',
+          fontWeight:'600'
+        }
+      ],
+      'title':[
+        '36px',
+        {
+          lineHeight:'54px',
+          fontWeight:'500'
+        }
+      ],
+      'nav':[
+        '24px',
+        {
+          lineHeight:'36px',
+          fontWeight:'500'
+        }
+      ],
+      'miniTitle':[
+        '24px',
+        {
+          lineHeight:'36px',
+          fontWeight:'500'
+        }
+      ],
+      'btn':[
+        '24px',
+        {
+          lineHeight:'36px',
+          fontWeight:'500'
+        }
+      ],
+      'body':[
+        '16px',{
+          lineHeight:'24px',
+          fontWeight:'400'
+        }
+      ]
+      
+    },
+    extend: {
+      padding:{
+        'btn':[
+          '55px 20px'
+        ]
+      },
+      width:{
+        'container':'1200px'
+      },
+      fontFamily:{
+        'Noto Serif Khmer':['Noto serif khmer']
+      },
+      borderRadius:{
+        'radius':'15px' 
+      },
+      margin:{
+        'top-content':'6rem',
+        'top-title':'2rem'
+      }
+    },
   },
   plugins: [],
 }
