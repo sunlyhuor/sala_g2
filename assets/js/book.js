1ea175ae4cd9ad4973ad0aa43ac1af3e65e0c8af
Helen <helen.leang15@example.com>
2023-12-24 15:41:48 +0700

    book and course index/view, book.js, getdata.js

--- assets/js/book.js
+++ assets/js/book.js
@@ -1,25 +1,5 @@
 
 'use strict'
-$(document).on("click", "#addtoFav", function () {
-    console.log("Add to fav clicked");
-    // Toggle the 'active' class
-    $(this).toggleClass("active");
-  
-    if ($(this).hasClass("active")) {
-      // Code to execute when the button is active (favorited)
-      $(this).html(`
-             <i class="fa-solid fa-heart text-2xl text-red-500"></i>
-          `);
-    
-    } else {
-      $(this).html(`
-            <i class="fa-regular fa-heart text-2xl"></i>
-      
-          `);
-    
-    }
-  });
-  
 const blog = document.getElementById("book");
 let output = "";
 const url = "https://cms.istad.co/api/sala-lessons?populate=thumbnail%2Cprofile";
@@ -35,21 +15,22 @@ const blockRender = (posts) => {
   `
   <div class="max-w-sm bg-white rounded-lg">
    <div class = "h-40"> 
-   <a href="/public/book/view.html?id=${post.id}">
-   <img class="rounded-t-lg  h-full w-full " src="${thumbnailUrl}" alt="">
+   <a href="/public/book/view.html">
+   <img class="rounded-t-lg h-full w-full object-contain" src="${thumbnailUrl}" alt="" id="imagesBook">
  </a>
    </div>
     <div class="p-5">
       <a href="/public/book/view.html">
-        <h5 class="mb-2 text-black text-2xl tracking-tight">${post.attributes.title}</h5>
+        <h5 class="mb-2 text-black text-2xl tracking-tight" id="title">${post.attributes.title}</h5>
       </a>
-      <p class="mb-3 text-black text-base tracking-tight text-des desc">${post.attributes.about}</p>
+      <p class="mb-3 text-black text-base tracking-tight text-des desc" id="about">${post.attributes.about}</p>
       <div class="flex items-center">
-        <img class="rounded-full w-9 h-9" src="${profileurl}" alt="profile picture">
+        <img class="rounded-full w-9 h-9" src="${profileurl}" alt="profile picture" id="imageProfile">
         <div class="w-full flex justify-between items-center">
           <div class=" text-start text-black text-sm font-medium font-['Noto Serif Khmer'] tracking-tight">
-            <div class="ps-3">${post.attributes.name}</div>
-            <div class="text-center text-black text-xs font-light font-['Noto Serif Khmer'] tracking-tight ps-3">${post.attributes.follower} ពាន់នាក់ ${dateOnly}</div>
+            <div class="ps-3" id="name">${post.attributes.name}</div>
+            <div class="text-center text-black text-xs font-light font-['Noto Serif Khmer'] tracking-tight ps-3"> <span id="follower">${post.attributes.follower} 
+            </span>ពាន់នាក់ <span id="dateOnly">${dateOnly}</span> </div>
           </div>
           <a href="#">
             <button id="addtoFav">
@@ -69,4 +50,67 @@ fetch(url)
   .then((jsonResult) => {
     let result = jsonResult.data;
     blockRender(result);
+  });
+
+
+
+
+  // add to favourtite in localStorage 
+  $(document).on("click", "#addtoFav", function () {
+    console.log("Add to fav clicked");
+  
+    // Toggle the 'active' class
+     $(this).toggleClass("active");
+  
+    if ($(this).hasClass("active")) {
+      // Code to execute when the button is active (favorited)
+      $(this).html(`
+      <i class="fa-solid fa-heart text-2xl text-red-500"></i>
+      `);
+  
+      // Find the parent card element
+      const cardElement = $(this).closest('.max-w-sm');
+  
+      // Extract data from the card
+      const title= cardElement.find('#title').text().trim();
+      const imagePf = cardElement.find('#imageProfile').attr('src').trim();
+      const imageBook = cardElement.find('#imagesBook').attr('src').trim();
+      const about = cardElement.find('#about').text().trim();
+      const name = cardElement.find('#name').text().trim();
+      const follower = cardElement.find('#follower').text().trim();
+      const dateOnly = cardElement.find('#dateOnly').text().trim();
+     
+  
+      // Prepare the data to be stored in localStorage
+      const storeData = {
+        title,
+        imagePf,
+        imageBook,
+        about,
+        name,
+        follower,
+        dateOnly
+      };
+      console.log("Extracted Data:", storeData);
+  
+      // Store data in localStorage
+      const favorites = JSON.parse(localStorage.getItem("favorites")) || [];
+      favorites.push(storeData);
+      localStorage.setItem("favorites", JSON.stringify(favorites));
+  
+    } else {
+      // Code to execute when the button is not active (not favorited)
+      $(this).html(`
+      <i class="fa-regular fa-heart text-2xl"></i>
+      `);
+  
+      // Code to execute when the button is not active (not favorited)
+      try {
+        // Attempt to store data in localStorage
+        localStorage.setItem("favorites", JSON.stringify(favorites));
+      } catch (e) {
+        // Handle the error (e.g., show a message to the user)
+        console.error("Error storing data in localStorage:", e);
+      }
+    }
   });
